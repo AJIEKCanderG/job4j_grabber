5c6b982755f57e2ff3bbcaa7cdfd2921e30568d0
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-10-24 20:33:32 +0300

    2.1.1. Парсинг 5 страниц https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -11,14 +11,19 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            Element date = td.parent().child(5);
-            System.out.println(date.text() + "\n");
+        for (int i = 1; i <= 5; i++) {
+            String url = "https://www.sql.ru/forum/job-offers/";
+            Document doc = Jsoup.connect(url + i).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                Element date = td.parent().child(5);
+                System.out.println(date.text());
+            }
+            System.out.println("\n" + "---------------------------- End page "
+                    + i + " ----------------------------------" + "\n");
         }
     }
 }
